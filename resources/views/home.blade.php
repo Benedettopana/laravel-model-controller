2d36bd66723c7404ec8c2ff3cf156ce781d3fe25
Benedettopana <benedetto.pana@gmail.com>
2024-05-10 15:43:16 +0200

    [add]Stampa card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,25 @@
 
 @section('content')
 
-<h1>Home</h1>
+<div class="container-xl">
+  <div class="row row-cols-3">
+    @foreach ($movie as $item)
+    <div class="col my-3">
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{ $item->title}}</h5>
+          <h6 class="card-subtitle mb-2 text-body-secondary">Titolo Originale: {{ $item->original_title }}</h6>
+          <p class="card-text">Nazionalità: {{ $item->nationality}}</p>
+          <p class="card-text">Data di uscita: {{ $item->date }}</p>
+          <p class="card-text">Voto: {{ $item->vote}}</p>
+
+        </div>
+      </div>
+    </div>
+    @endforeach
+  </div>
+
+</div>
+
 
 @endsection
